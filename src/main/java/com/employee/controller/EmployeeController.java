30db351454f2087c0ffa2d659830a7fc06a5fb1f
haseeAmarathunga <haseeamarathunga@gmail.com>
2019-09-02 12:26:03 +0530

    Intialize

---
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -0,0 +1,58 @@
+package com.employee.controller;
+
+import com.employee.exception.ResourceNotFoundException;
+import com.employee.model.Employee;
+import com.employee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @GetMapping("/employee")
+    public List<Employee> getAllEmployees()
+    {
+        return employeeRepository.findAll();
+    }
+
+    @GetMapping("/employee/{id}")
+    public Employee getEmployee(@PathVariable(name = "id") long id)
+    {
+        return employeeRepository.findById(id) .orElseThrow(() -> new ResourceNotFoundException("Employee","id",id));
+    }
+
+    @PostMapping("/employee")
+    public Employee saveEmployee(@RequestBody Employee employee)
+    {
+        return employeeRepository.save(employee);
+    }
+
+    @PutMapping("/employee/{id}")
+    public Employee editEmployee(@PathVariable(value = "id") Long id,@RequestBody Employee employee)
+    {
+        Employee employee1 = employeeRepository.findById(id) .orElseThrow(() -> new ResourceNotFoundException("Employee", "id", id));
+        employee1.setAddress(employee.getAddress());
+        employee1.setEmail(employee.getEmail());
+        employee1.setFullname(employee.getFullname());
+        employee1.setMobile(employee.getMobile());
+        employee1.setNic(employee.getNic());
+        employee1.setPassword(employee.getPassword());
+
+        Employee updatedEmployee = employeeRepository.save(employee1);
+        return updatedEmployee;
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable(value = "id") Long id)
+    {
+        Employee employee = employeeRepository.findById(id) .orElseThrow(() -> new ResourceNotFoundException("Employee","id",id));
+        employeeRepository.delete(employee);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/employee/model/Employee.java
@@ -0,0 +1,105 @@
+package com.employee.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    long id;
+
+    @Column(name = "FullName")
+    String fullname;
+
+    @Column(name = "Nic")
+    String nic;
+
+    @Column(name = "Address")
+    String address;
+
+    @Column(name = "Mobile")
+    String mobile;
+
+    @Column(name = "Email")
+    String email;
+
+    @Column(name = "Password")
+    String password;
+
+    public Employee() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", fullname='" + fullname + '\'' +
+                ", nic='" + nic + '\'' +
+                ", address='" + address + '\'' +
+                ", mobile='" + mobile + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
